10447073df7f2a9e7178596befd7e95245c065b8
Chali Musonda <51807287+mcmusonda@users.noreply.github.com>
2025-02-26 08:42:58 +0200

    Initial project commit

---
+++ database/migrations/2024_04_16_153514_create_loans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('member_id');
+            $table->foreignId('meeting_id');
+            $table->datetime('date_requested');
+            $table->decimal('requested_amount');
+            $table->datetime('date_paid');
+            $table->decimal('amount_paid', 9, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
---
+++ database/migrations/2024_04_16_153525_create_shares_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shares', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('member_id');
+            $table->foreignId('meeting_id');
+            $table->datetime('date_paid');
+            $table->decimal('shares_bought', 9, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shares');
+    }
+};
